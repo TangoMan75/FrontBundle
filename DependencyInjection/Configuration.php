e3ee5799a1525b78f7ca5b6227633a90de42249a
Matthias Morin <TangoMan75@users.noreply.github.com>
2018-03-19 15:53:08 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tangoman_list_manager');
+        $rootNode = $treeBuilder->root('tangoman_front');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
